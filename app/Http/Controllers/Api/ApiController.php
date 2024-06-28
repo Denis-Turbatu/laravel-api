be9c3a9fbc96caab8992d1ea90376fe8a241ab94
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-28 13:37:11 +0200

    feat: index() method in ApiController, retrieve data and passed in formatted package

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -12,54 +13,16 @@ class ApiController extends Controller
      */
     public function index()
     {
-        //
+        // Estrazione di tutti i progetti in $project
+        $projects = Project::all();
+
+        // impacchettamento di $project e formattazione della response API
+        $data = [
+            'result' => $projects,
+        ];
+
+        // ritorno una risposta in formatto json del pacchetto formattato
+        return response()->json($data);    
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
