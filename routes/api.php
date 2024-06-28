b3d7d21fa73d84b959c795d9b705830cf2e701d1
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-28 13:45:19 +0200

    feat: setted Route in web.php for api call

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// nel URL '/projects' richiamo il metodo index() presente in ProjectController
+Route::get('/projects', [ProjectController::class, 'index']);
